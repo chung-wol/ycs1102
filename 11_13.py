c38fce7d0115fe35341d0ce475f9f1d4d0d9861e
Choi Byung Jun <45877346+cbj0523@users.noreply.github.com>
2025-07-07 12:07:40 +0900

    gitignore fixed

--- 11_13.py
+++
@@ -1,5 +0,0 @@
-a = int(input("밑변: "))
-b = int(input("윗변: "))
-h = int(input("높이: "))
-s = 1/2*(a+b)*h
-print(s)
--- readnwrite.py
+++
@@ -1,8 +0,0 @@
-f = open('\link', 'r')
-out = open('\link', 'w')
-
-for line in f:
-    email_addr = line
-    at_index = email_addr.find('@')
-    user_name = email_addr[0:at_index]
-    
